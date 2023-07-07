50f64e804914395a48561b365765794cf4f6b050
Leonardo <leo.abreudp@gmail.com>
2023-07-07 14:33:45 +0100

    first configs for my main website

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Navbar from '../components/navbar'
+import Footer from '../components/footer'
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -15,8 +14,14 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className='h-full'>
+      <body className="bg-theme text-white relative w-full h-full">
+        <Navbar/>
+        <main className="pt-16 xl:px-72">
+          {children}
+        </main>
+        <Footer/>   
+      </body>
     </html>
   )
 }
